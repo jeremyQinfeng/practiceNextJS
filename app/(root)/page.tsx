80022550e246cb037e286a148969a1d6656b9512
jeremy <jeremyqinfeng@outlook.com>
2025-06-06 18:45:18 +0800

    7

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    <h1 className="text-2xl">HOME</h1>
-    </>
-      
+    <div className="flex items-center justify-center">
+      <div className="uppercase bg-black px-6 py-3 font-worksans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5">
+        HOME
+      </div>
+    </div>
   );
 }
